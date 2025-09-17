2cc9fb64e4f47dc6f0b5ac886714e8cbe4092bf2
Sergey Nesterenko <sergey.nesterenko@edgecenter.ru>
2025-09-17 21:58:38 +0300

    Initialize Vulkan project scaffold, add build system, and configuration files.

---
+++ src/main.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+const c = @import("clibs.zig");
+
+const vulkan_init = @import("vulkan_init.zig");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
+    defer if (gpa.deinit() == .leak) {
+        @panic("Leaked memory");
+    };
+
+    const allocator = gpa.allocator();
+
+    const required_extensions = [_][*]const u8{
+        c.vk.KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME, // only required for Vulkan version 1.0.
+        c.vk.KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME,
+    };
+
+    const vk_alloc_cbs: ?*c.vk.AllocationCallbacks = null;
+    const instance = try vulkan_init.create_instance(allocator, .{
+        .application_name = "VkGuide",
+        .application_version = c.vk.MAKE_VERSION(0, 1, 0),
+        .engine_name = "VkGuide",
+        .engine_version = c.vk.MAKE_VERSION(0, 1, 0),
+        .api_version = c.vk.MAKE_VERSION(1, 1, 0),
+        .debug = true,
+        .alloc_cb = vk_alloc_cbs,
+        .required_extensions = &required_extensions,
+    });
+
+    c.vk.DestroyInstance(instance.handle, vk_alloc_cbs);
+}
