c7f78e71bdea452030a63ceb57cce5a17c32f168
Sergey Nesterenko <nestslock@gmail.com>
2025-10-20 14:52:03 +0300

    Refactor Vulkan shader management: add `ShaderModule` struct with `create`, `destroy`, and initialization methods; integrate shader modules into graphics pipeline creation

--- src/vulkan/gpipeline.zig
+++ src/vulkan/gpipeline.zig
@@ -1,10 +1,17 @@
 const std = @import("std");
+
+const c = @import("clibs.zig");
 const config = @import("config");
 const shaders = @import("shaders.zig");
+const devices = @import("devices.zig");
 
 
-pub fn createGraphicsPipeline(alloc: std.mem.Allocator) !void {
-    var stack_fallback = std.heap.stackFallback(50 * @sizeOf(u8), alloc);
+pub fn createGraphicsPipeline(
+    alloc: std.mem.Allocator,
+    device: *devices.Device,
+    alloc_cbs: ?*c.vk.AllocationCallbacks,
+) !void {
+    var stack_fallback = std.heap.stackFallback(100 * @sizeOf(u8), alloc);
     const sfa_alloc = stack_fallback.get();
 
     const vert_path = try std.fs.path.join(sfa_alloc, &.{ config.shaders_path, "vert.spv" });
@@ -12,8 +19,14 @@ pub fn createGraphicsPipeline(alloc: std.mem.Allocator) !void {
     const vert_shader_code = try shaders.loadCode(alloc, vert_path);
     defer alloc.free(vert_shader_code);
 
-    const frag_path = try std.fs.path.join(sfa_alloc, &.{ config.shaders_path, "vert.spv" });
+    const vert_shader_module = try shaders.ShaderModule.create(alloc, device, alloc_cbs, vert_shader_code);
+    defer vert_shader_module.destroy();
+
+    const frag_path = try std.fs.path.join(sfa_alloc, &.{ config.shaders_path, "frag.spv" });
     defer sfa_alloc.free(frag_path);
     const frag_shader_code = try shaders.loadCode(alloc, frag_path);
     defer alloc.free(frag_shader_code);
+
+    const frag_shader_module = try shaders.ShaderModule.create(alloc, device, alloc_cbs, frag_shader_code);
+    defer frag_shader_module.destroy();
 }
--- src/vulkan/shaders.zig
+++ src/vulkan/shaders.zig
@@ -1,6 +1,8 @@
-
 const std = @import("std");
 
+const c = @import("clibs.zig");
+const devices = @import("devices.zig");
+const checkVk = @import("errors.zig").checkVk;
 
 pub fn loadCode(alloc: std.mem.Allocator, file_path: []const u8) ![]u8 {
     var file = try std.fs.openFileAbsolute(file_path, .{});
@@ -24,3 +26,50 @@ pub fn loadCode(alloc: std.mem.Allocator, file_path: []const u8) ![]u8 {
 
     return buffer;
 }
+
+pub const ShaderModule = struct {
+    const Self = @This();
+
+    alloc: std.mem.Allocator,
+    handle: c.vk.ShaderModule,
+    device: c.vk.Device,
+    alloc_cbs: ?*c.vk.AllocationCallbacks = null,
+
+    pub fn init(alloc: std.mem.Allocator, device: *devices.Device, alloc_cbs: ?*c.vk.AllocationCallbacks, code: []const u8) !Self {
+        if (!std.mem.isAligned(@intFromPtr(code.ptr), 4)) return error.BadAlignment;
+
+        const create_info = std.mem.zeroInit(c.vk.ShaderModuleCreateInfo, .{
+            .sType = c.vk.STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+            .codeSize = code.len,
+            .pCode = @as([*c]const u32, @ptrCast(@alignCast(code.ptr))),
+        });
+
+        var module: c.vk.ShaderModule = undefined;
+        try checkVk(c.vk.CreateShaderModule(device.handle, &create_info, alloc_cbs, &module));
+
+        return .{
+            .alloc = alloc,
+            .handle = module,
+            .device = device.handle,
+            .alloc_cbs = alloc_cbs,
+        };
+    }
+
+    pub fn create(alloc: std.mem.Allocator, device: *devices.Device, alloc_cbs: ?*c.vk.AllocationCallbacks, code: []const u8) !*Self {
+        const self = try alloc.create(ShaderModule);
+        errdefer alloc.destroy(self);
+
+        self.* = try ShaderModule.init(alloc, device, alloc_cbs, code);
+        return self;
+    }
+
+    pub fn deinit(self: *Self) void {
+        c.vk.DestroyShaderModule(self.device, self.handle, self.alloc_cbs);
+    }
+
+    pub fn destroy(self: *Self) void {
+        const allocator = self.alloc;
+        self.deinit();
+        allocator.destroy(self);
+    }
+};
